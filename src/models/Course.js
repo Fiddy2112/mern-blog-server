b3fb243183de1c91fd0fffaa83f395dd23548817
Fiddy <85234199+Felixx2112@users.noreply.github.com>
2022-09-10 22:38:25 +0700

    create-slug & and config deloy to server

--- src/models/Course.js
+++ src/models/Course.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 const Schema = mongoose.Schema;
 
-const CourseSchema = new Schema({
-  name: { type: String, required: true },
-  description: { type: String },
-  image: { type: String },
-  videoId: { type: String, required: true },
-  status: { type: String, enum: ["TO LEARN", "LEARNING", "LEARNED"] },
-  user: { type: Schema.Types.ObjectId, ref: "users" },
-  createAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
-});
+const CourseSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    videoId: { type: String, required: true },
+    status: { type: String, enum: ["TO LEARN", "LEARNING", "LEARNED"] },
+    user: { type: Schema.Types.ObjectId, ref: "users" },
+    createAt: { type: Date, default: Date.now },
+    updateAt: { type: Date, default: Date.now },
+    slug: { type: String, slug: "name", unique: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("courses", CourseSchema);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const verifyToken = require("../middleware/auth");
 const userController = require("../controllers/UserController");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.get("/", verifyToken, userController.userLogged);
 
 module.exports = router;
